4d3216dfd55aa697345a49314983e64fb886ff81
Maxime from Nightborn <44728919+m-Nightly@users.noreply.github.com>
2023-01-10 21:31:32 +0100

    chore: project cards

--- src/pages/projects/[projectId]/[...detail]/index.tsx
+++ src/pages/projects/[projectId]/[...detail]/index.tsx
@@ -1,5 +1,10 @@
 import ProjectLayout from "@components/navigation/layout";
+import { useGetProjects } from "src/services/projects/projects";
 
 export default function Home() {
-	return <ProjectLayout />;
+	// Attributes
+	const { data: projects } = useGetProjects("2a510101-7a88-4ac7-83a7-c5662434f564");
+
+	// Render
+	return <ProjectLayout projects={projects} />;
 }
