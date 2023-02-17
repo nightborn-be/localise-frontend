b7a05f8538bfb4fbd64db214ca18556e3ac60011
YoussefNight <Youssef.El.Majdoul@NightBorn.com>
2023-02-17 12:02:23 +0100

    feat: Continue to make component button and stories

--- libs/ui/components/inputs/button/index.stories.tsx
+++ libs/ui/components/inputs/button/index.stories.tsx
@@ -22,11 +22,20 @@ const props: IButtonProps = {
     borderRadius: '8px',
     w: '66px',
     h: '32px',
-    startEnHancer: <Icon name='add' stroke='#FFFFFF' width='16' height='16' />,
+    startEnHancer: (
+        <Icon
+            name='add'
+            stroke='#FFFFFF'
+            width='16'
+            height='16'
+            viewBox='0 0 16 16'
+        />
+    ),
     fontWeight: 600,
     lineHeight: '17px',
     textAlign: 'center',
     spacing: '4px',
+    hoverBackgroundColor: '#4C36B5',
     onClick: () => {},
 };
 
@@ -43,13 +52,22 @@ export const LargeTextRightIconNoBackground = Template.bind({});
 LargeTextRightIconNoBackground.args = {
     ...props,
     fontSize: '14px',
-    w: '74px',
-    h: '40px',
+    w: '66px',
+    h: '36px',
+    lineHeight: '15px',
     color: '#5F43E2',
     startEnHancer: undefined,
-    endEnHancer: <Icon name='add' stroke='#5F43E2' width='16' height='16' />,
+    endEnHancer: (
+        <Icon
+            name='add'
+            stroke='#5F43E2'
+            width='16'
+            height='16'
+            viewBox='0 0 16 16'
+        />
+    ),
     backgroundColor: 'transparent',
-    hoverBackgroundColor: 'lightgray',
+    hoverBackgroundColor: '#EDEEFC',
 };
 export const TextWithBorder = Template.bind({});
 TextWithBorder.args = {
@@ -57,12 +75,21 @@ TextWithBorder.args = {
     border: '1px solid #5F43E2',
     borderRadius: '8px',
     backgroundColor: '#F8F9FC',
-    startEnHancer: <Icon name='add' stroke='#5F43E2' width='16' height='16' />,
+    startEnHancer: (
+        <Icon
+            name='add'
+            stroke='#5F43E2'
+            width='16'
+            height='16'
+            viewBox='0 0 16 16'
+        />
+    ),
     color: '#5F43E2',
+    hoverBackgroundColor: '#EDEEFC',
     icon: undefined,
 };
-export const TextIconWithBorderDashed = Template.bind({});
-TextIconWithBorderDashed.args = {
+export const TextRightIconWithBorderDashed = Template.bind({});
+TextRightIconWithBorderDashed.args = {
     ...props,
     border: '1px dashed #8F95B2',
     borderRadius: '8px',
@@ -70,7 +97,16 @@ TextIconWithBorderDashed.args = {
     color: '#8F95B2',
     lineHeight: '17px',
     startEnHancer: undefined,
-    endEnHancer: <Icon name='add' stroke='#8F95B2' width='16' height='16' />,
+    hoverBackgroundColor: undefined,
+    endEnHancer: (
+        <Icon
+            name='add'
+            stroke='#8F95B2'
+            width='16'
+            height='16'
+            viewBox='0 0 16 16'
+        />
+    ),
 };
 const trashProps: IButtonProps = {
     text: 'Text',
@@ -87,6 +123,7 @@ const trashProps: IButtonProps = {
             fill='#FFFFFF'
             width='12'
             height='12'
+            viewBox='0 0 12 12'
         />
     ),
     fontWeight: 600,
@@ -94,11 +131,13 @@ const trashProps: IButtonProps = {
     textAlign: 'center',
     spacing: '4px',
     onClick: () => {},
+    hoverBackgroundColor: '#E55252',
 };
 
 export const TrashText = Template.bind({});
 TrashText.args = {
     ...trashProps,
+    icon: undefined,
     startEnHancer: undefined,
     endEnHancer: undefined,
 };
@@ -106,29 +145,49 @@ TrashText.args = {
 export const TrashRightWithText = Template.bind({});
 TrashRightWithText.args = {
     ...trashProps,
+    fontSize: '14px',
+    lineHeight: '17px',
     w: '76px',
     h: '44px',
+    startEnHancer: (
+        <Icon
+            name='trash'
+            stroke='#FFFFFF'
+            fill='#FFFFFF'
+            width='14'
+            height='14'
+            viewBox='0 0 14 14'
+        />
+    ),
+    padding: '4px 12px',
+    gap: '8px',
     spacing: '8.33px',
 };
 export const TrashLeftWithTextNoBackground = Template.bind({});
 TrashLeftWithTextNoBackground.args = {
     ...trashProps,
     color: '#F46363',
+    fontSize: '14px',
+    lineHeight: '15px',
+    fontWeight: 600,
     startEnHancer: undefined,
     endEnHancer: (
         <Icon
             name='trash'
             stroke='##F46363'
             fill='#F46363'
-            width='16'
-            height='16'
+            width='14'
+            height='14'
+            viewBox='0 0 14 14'
         />
     ),
     w: '76px',
-    h: '44px',
-    lineHeight: '15px',
+    h: '40px',
+    padding: '4px 12px',
+    gap: '8px',
     spacing: '8.33px',
     backgroundColor: 'transparent',
+    hoverBackgroundColor: '#F2F4F9',
 };
 
 export const AddMember = Template.bind({});
@@ -152,7 +211,7 @@ AddMember.args = {
 export const AddPicture = Template.bind({});
 
 AddPicture.args = {
-    border: '1px dashed #8F95B2',
+    border: '2px dashed #8F95B2',
     borderRadius: '16px',
     text: 'Add a picture',
     w: '100px',
@@ -162,6 +221,7 @@ AddPicture.args = {
     direction: 'column',
     backgroundColor: 'transparent',
     color: '#8F95B2',
+    whiteSpace: 'pre-line',
     startEnHancer: (
         <Icon name='uploadCloud' stroke='#8F95B2' width='24' height='24' />
     ),
