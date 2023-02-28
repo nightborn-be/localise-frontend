f71cb529de5e3a6799a5254fec5cdb6033dcb43d
YoussefNight <Youssef.El.Majdoul@NightBorn.com>
2023-02-28 09:59:02 +0100

    fix: Pull Request update my code

--- libs/ui/components/inputs/button-children/props.ts
+++ libs/ui/components/inputs/button-children/props.ts
@@ -3,7 +3,7 @@ import { ButtonProps, StackDirection } from "@chakra-ui/react";
 export default interface IButtonChildrenProps extends ButtonProps {
     hoverColor?: string,
     direction?: StackDirection,
-    isHovered: boolean | undefined,
+    isHovered: boolean,
     children?: React.ReactElement | string;
     color?: string;
 }
