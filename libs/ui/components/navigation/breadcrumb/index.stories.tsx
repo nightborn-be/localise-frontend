49525186021a630ba96dddc6aaefb0bc9916aa20
YoussefNight <Youssef.El.Majdoul@NightBorn.com>
2023-03-09 16:54:35 +0100

    feat: end of Navigation Component

---
+++ libs/ui/components/navigation/breadcrumb/index.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import React from 'react';
+import { Breadcrumb } from '.';
+import BreadcrumbProps from './props';
+
+
+export default {
+    title: 'Components/Inputs/Breadcrumb',
+    component: Breadcrumb,
+} as ComponentMeta<typeof Breadcrumb>;
+const Template: ComponentStory<typeof Breadcrumb> = (
+    props: BreadcrumbProps,
+) => <Breadcrumb {...props} />;
+
+export const Default = Template.bind({});
+Default.args = {
+    elements: [
+        {
+            label: 'Project',
+        },
+        {
+            label: 'Mobile App',
+        },
+    ],
+};
