ca69d7963f718743bac1d4ab4275584af037b0d5
YoussefNight <Youssef.El.Majdoul@NightBorn.com>
2023-03-10 10:41:23 +0100

    feat: end of role component

--- libs/ui/components/contents/role/index.stories.tsx
+++ libs/ui/components/contents/role/index.stories.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import IRoleProps from './props';
-import Role from '.';
 import COLORS from '../../../constants/colors';
-
+import Role from './index';
 export default {
     title: 'Components/Constents/Role',
     component: Role,
@@ -12,23 +11,23 @@ const Template: ComponentStory<typeof Role> = (props: IRoleProps) => (
     <Role {...props} />
 );
 
-const Admin = Template.bind({})
-Admin.args = { 
-    text:"Admin",
+export const Admin = Template.bind({});
+Admin.args = {
+    text: 'Admin',
     color: COLORS.Localize.Purple.T500.value,
-    backgroudColor: COLORS.Localize.Purple.T100.value,
+    backgroundColor: COLORS.Localize.Purple.T100.value,
 };
 
-const Developer = Template.bind({});
+export const Developer = Template.bind({});
 Developer.args = {
     text: 'Developer',
     color: COLORS.Warning.T500.value,
-    backgroudColor: COLORS.Warning.T100.value,
+    backgroundColor: COLORS.Warning.T100.value,
 };
 
-const Translator = Template.bind({});
+export const Translator = Template.bind({});
 Translator.args = {
     text: 'Translator',
     color: COLORS.Text.T500.value,
-    backgroudColor: COLORS.Line.value
+    backgroundColor: COLORS.Line.value,
 };
--- libs/ui/components/contents/role/props.ts
+++ libs/ui/components/contents/role/props.ts
@@ -1,5 +1,3 @@
-import { IconButtonProps } from '@chakra-ui/react';
-import React from 'react'
 export default interface IRoleProps  {
     text?:string,
     color?: string,
