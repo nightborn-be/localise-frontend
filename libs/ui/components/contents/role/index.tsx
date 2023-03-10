a2c8e4c4a11ee40cdc03838d64d5fecc2c18055e
YoussefNight <Youssef.El.Majdoul@NightBorn.com>
2023-03-10 10:41:23 +0100

    feat: end of role component

--- libs/ui/components/contents/role/index.tsx
+++ libs/ui/components/contents/role/index.tsx
@@ -1,18 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import IRoleProps from './props';
 import FONTS from '../../../constants/fonts';
 import Text from '../text';
 import { HStack } from '@chakra-ui/react';
 
-export default function Role({
-    text,
-    color,
-    backgroundColor,
-}: IRoleProps) {
+export default function Role({ text, color, backgroundColor }: IRoleProps) {
     // Render
     return (
-        <HStack bg={backgroundColor}>
-            <Text font={FONTS.T1.T12px.Medium500.value} color={color}>{text}</Text>
+        <HStack
+            backgroundColor={backgroundColor}
+            py='4px'
+            px='8px'
+            width='fit-content'
+            borderRadius={'4px'}
+        >
+            <Text font={FONTS.T1.T12px.Medium500.value} color={color}>
+                {text}
+            </Text>
         </HStack>
     );
 }
