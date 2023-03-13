9072657660ec2286b45a7a04d081a31f98fb5d71
YoussefNight <Youssef.El.Majdoul@NightBorn.com>
2023-03-13 10:53:23 +0100

    fix: code update

--- libs/ui/components/navigation/tabs/tab/index.tsx
+++ libs/ui/components/navigation/tabs/tab/index.tsx
@@ -3,18 +3,19 @@ import { ITabProps } from './props';
 import COLORS from '../../../../constants/colors';
 import Text from '../../../contents/text';
 import FONTS from '../../../../constants/fonts';
+import { Box } from '@chakra-ui/react';
 
 const Tab = ({ children, onSelect, active }: ITabProps): JSX.Element => {
     // Render
     return (
-        <Text
-            cursor='pointer'
-            onClick={onSelect}
-            font={FONTS.T1.T12px.Medium500.value}
-            color={active ? COLORS.Text.T400.value : COLORS.InputText.value}
-        >
-            {children}
-        </Text>
+        <Box cursor='pointer' onClick={onSelect}>
+            <Text
+                font={FONTS.T1.T12px.Medium500.value}
+                color={active ? COLORS.Text.T400.value : COLORS.InputText.value}
+            >
+                {children}
+            </Text>
+        </Box>
     );
 };
 
