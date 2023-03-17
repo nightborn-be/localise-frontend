73d55d667b4c5afa122c42f14fcd5daef53cb52e
YoussefNight <Youssef.El.Majdoul@NightBorn.com>
2023-03-17 17:40:52 +0100

    feat: change the search bar create the multi and select searchbar

--- libs/ui/components/inputs/searchbar-select/props.ts
+++ libs/ui/components/inputs/searchbar-select/props.ts
@@ -1,12 +1,12 @@
 import { InputProps } from '@chakra-ui/react';
-export default interface SearchbarProps<T> extends InputProps {
+export default interface ISearchbarSelectProps<T> extends InputProps {
     placeholderColor?: string,
     marginLeftElement?: string,
+    activeKey?: T,
+    filterValue?: string,
     spacing?: string,
-    values?: Options<T>[]
     options?: Options<T>[]
-    activeKeys?: T[]
-    onCheck?: (values: T) => void
+    onSelect?: (value) => void
 }
 
 export type Options<T> = {
