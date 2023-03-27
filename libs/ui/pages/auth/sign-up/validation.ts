7d2433831f043a911fd679847429dd839b4f71bd
YoussefNight <Youssef.El.Majdoul@NightBorn.com>
2023-03-27 11:47:13 +0200

    feat: change email error msg

--- libs/ui/pages/auth/sign-up/validation.ts
+++ libs/ui/pages/auth/sign-up/validation.ts
@@ -1,7 +1,7 @@
 import * as Yup from 'yup';
 
 const schema = Yup.object({
-    email: Yup.string().email().required('Email is required'),
+    email: Yup.string().email('Email must be a valid email').required('Email is required'),
     password: Yup.string().required('Password is required'),
 });
 
