5bde211ef65c05ad288f6034e8a093e6e6a7393d
ThomasNight <thomas.michel@nightborn.be>
2023-03-31 13:30:14 +0200

    fix: translations

--- libs/i18n/keys.ts
+++ libs/i18n/keys.ts
@@ -3,44 +3,6 @@
 export const tKeys = {
     common: {
         test: 'common.test',
-        buttonLogin: 'common.buttonLogin',
-    },
-    home: {
-        welcom: {
-            title: 'home.welcom.title',
-        },
-        buttonSignup: 'home.buttonSignup',
-        subtitle: 'home.subtitle',
-    },
-    login: {
-        title: 'login.title',
-        inputLabel: {
-            password: 'login.inputLabel.password',
-            email: 'login.inputLabel.email',
-        },
-        inputPlaceholder: {
-            password: 'login.inputPlaceholder.password',
-            email: 'login.inputPlaceholder.email',
-        },
-    },
-    signup: {
-        account: {
-            title: 'signup.account.title',
-        },
-        inputLabel: {
-            organizationName: 'signup.inputLabel.organizationName',
-        },
-        title: {
-            organizationName: 'signup.title.organizationName',
-        },
-        buttonContinue: 'signup.buttonContinue',
-        organizationName: {
-            title: 'signup.organizationName.title',
-        },
-        inputPlaceholder: {
-            organizationName: 'signup.inputPlaceholder.organizationName',
-        },
-        buttonLater: 'signup.buttonLater',
     },
     auth: {
         home: {
