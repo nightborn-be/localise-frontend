83658705edf761d03b18a7d1c471e63e5fbca6b3
YoussefNight <Youssef.El.Majdoul@NightBorn.com>
2023-05-05 11:22:27 +0200

    fix: pr review

--- libs/ui/pages/mappers.ts
+++ libs/ui/pages/mappers.ts
@@ -8,36 +8,23 @@ export const toUpdateProjectDTO = (
     projectName: string,
     sourceLanguage: string,
     targetLanguages: string[],
-): UpdateProjectDTO => {
+): UpdateProjectDTO | undefined => {
     const source = languages.find(language => language.name === sourceLanguage)
     const targets = targetLanguages.map(language => languages.find(lang => lang.name === language))
+    if (source === undefined) {
+        return undefined
+    }
+    if (targets === undefined) {
+        return undefined
+    }
     const res: UpdateProjectDTO = {
         name: projectName,
-        sourceLanguage: toUpsertProjectLanguageDTO(source),
-        languages: toUpsertProjectLanguageDTOArray(targets)
+        sourceLanguage: source,
+        languages: toUpsertProjectLanguageDTOArray()
     };
     return res;
 };
 
-
-const toUpsertProjectLanguageDTO = (object?: UpsertProjectLanguageDTO): UpsertProjectLanguageDTO => {
-    if (object === undefined) {
-        return { name: '', abbreviation: '' }
-    }
-    return object
-}
-
-const toUpsertProjectLanguageDTOArray = (object: (UpsertProjectLanguageDTO | undefined)[]): UpsertProjectLanguageDTO[] => {
-    if (object === undefined) {
-        return []
-    }
-    let newObject: UpsertProjectLanguageDTO[] = []
-    for (let index = 0; index < object.length; index++) {
-        const element = object[index];
-        if (element) {
-            newObject[index] = element
-        }
-
-    }
-    return newObject;
+const toUpsertProjectLanguageDTOArray = (): UpsertProjectLanguageDTO[] => {
+    return languages.filter(language => language !== undefined) as UpsertProjectLanguageDTO[]
 }
